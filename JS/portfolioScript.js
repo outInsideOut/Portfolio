6ef8bd13ef11764705afb06fd849e31ca6a0c313
FOC <finred@hotmail.co.uk>
2021-01-22 20:14:39 +0000

    Added some bits :)

--- JS/portfolioScript.js
+++ JS/portfolioScript.js
@@ -37,7 +37,9 @@ $(document).ready(function(){
 	$('nav > .5').click(function() {
 		window.open('https://www.linkedin.com/in/fionn-o-connor-a117071b7/', '_blank');
 	});
-	
+	$('#dreamingSpires .image').click(function() {
+		window.open('https://dreamingspires.dev', '_blank');
+	});
 	$('#fractions .image').click(function() {
 		window.open('https://github.com/outInsideOut/fractions', '_blank');
 	});
